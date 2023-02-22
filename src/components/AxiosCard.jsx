57dbdb26e81ba4a14832b5d145a86b3fab17eb30
hugopracticas <hugoloyolamaya@gmail.com>
2023-02-22 14:25:43 -0600

    cambio en estilos

--- src/components/AxiosCard.jsx
+++ src/components/AxiosCard.jsx
@@ -5,10 +5,11 @@ export const AxiosCard = ({axiosCharacters}) => {
   return (
     <div className='containerCard'>
       {axiosCharacters.map((character) => (
-        <div className='cardCharacter' key={character.id}>
+        <div className={character.gender === 'Male' ? 'cardCharacterMale' : 'cardCharacterFemale'} key={character.id}>
           <img src={character.image}/>
           <h2>{character.name}</h2>
           <p>Location:{character.location}</p>
+          <p>Gender:{character.gender}</p>
         </div>
       ))
       }
